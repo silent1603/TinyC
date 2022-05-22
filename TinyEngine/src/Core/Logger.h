213fe8d28d7c8094b37f539ee0cdf0d5c8289766
silent1603 <tamgiangbui@gmail.com>
2022-05-22 16:37:26 +0700

    update cmake

---
+++ TinyEngine/src/Core/Logger.h
@@ -0,0 +1,13 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "define.h"
+typedef enum log_level{
+    LOG_LEVEL_FATAL = 0,
+    LOG_LEVEL_ERROR = 1,
+    LOG_LEVEL_WARN = 2,
+    LOG_LEVEL_INFO = 3,
+    LOG_LEVEL_DEBUG = 4,
+    LOG_LEVEL_TRACE = 5
+} log_level;
+#endif
